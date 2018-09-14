82da5e3bc69023399152fb1c0a234a03682687bf
Unknown <jjzwink@gmail.com>
2018-09-14 19:07:53 -0400

    setting up Ui for Torture script
    setting the dialog and folder for the HUD for torturing

---
+++ kp_liberation_WolfeLabs.Takistan/ui/torture/MyDialog.h
@@ -0,0 +1,31 @@
+//Exported via Arma Dialog Creator (https://github.com/kayler-renslow/arma-dialog-creator)
+
+#include "TortureHUD.h"
+class MyDialog
+{
+	idd = -1;
+	
+	class ControlsBackground
+	{
+		
+	};
+	class Controls
+	{
+		class target_life
+		{
+			type = 8;
+			idc = -1;
+			x = safeZoneX + safeZoneW * 0.29394532;
+			y = safeZoneY + safeZoneH * 0.22569445;
+			w = safeZoneW * 0.41210938;
+			h = safeZoneH * 0.03125;
+			style = 0;
+			colorBar[] = {1,0,0,1};
+			colorFrame[] = {0.3373,0.451,0.9882,1};
+			texture = "#(argb,8,8,3)color(1,1,1,1)";
+			
+		};
+		
+	};
+	
+};
---
+++ kp_liberation_WolfeLabs.Takistan/ui/torture/TortureHUD.h
@@ -0,0 +1,6 @@
+//Exported via Arma Dialog Creator (https://github.com/kayler-renslow/arma-dialog-creator)
+#ifndef HG_TortureHUDh
+#define HG_TortureHUDh 1
+//Create a header guard to prevent duplicate include.
+
+#endif
